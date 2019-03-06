319fc16ab55de6864c3ff6e3fb124382abb32fb5
Crystal Neal <cdneal091@gmail.com>
2019-03-06 18:17:23 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,12 +12,18 @@ songs = [
 ]
 
 def help
+  puts 'I accept the following commands:'
+  puts '- help : displays this help message'
+  puts '- list : displays a list of songs you can play'
+  puts '- exit : exits this program'
 end
 
 def play
 end
 
-def list
+def list(songs)
+  songs.each_with_index do | song , i |
+    puts "#{i+1}. #{song}"
 end
 
 def exit_jukebox
